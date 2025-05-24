fcd2fd3364b5ba5f368990915ff8bdcf9e74d547
kimura79 <ema.saracino@gmail.com>
2025-05-25 00:20:16 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
-from predict import predict
+from cog import BasePredictor, Input, Path
+from PIL import Image
+from predict import Predictor
 
-def run(inputs):
-    image = inputs["image"]
-    eta = inputs.get("eta", None)
-    return predict(image, eta)
+class PredictorCog(BasePredictor):
+    def setup(self):
+        self.predictor = Predictor()
+
+    def predict(
+        self,
+        image: Path = Input(description="Immagine del volto"),
+        eta_input: str = Input(description="Età stimata utente", default=None)
+    ) -> dict:
+        image = Image.open(image).convert("RGB")
+        return self.predictor.predict(image, eta_input)
